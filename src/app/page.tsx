ace63ce57220207c592bb1b4f18dea69668c8f64
Erick <erickcestari03@gmail.com>
2023-08-21 17:11:04 -0300

    Adding button to send user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 "use client"
-import Typography from '@mui/material/Typography'
 import TextField from '@mui/material/TextField'
+import InputAdornment from '@mui/material/InputAdornment'
 import { useTheme } from '@mui/material/styles'
 import { ChangeEvent, useState } from 'react'
+import AccountCircleOutline from 'mdi-material-ui/AccountCircleOutline'
+import SendCircleOutline  from 'mdi-material-ui/SendCircleOutline'
+import { IconButton } from '@mui/material'
 
 export default function Home() {
   const theme = useTheme()
@@ -22,18 +25,35 @@ export default function Home() {
             <p className="md:text-4xl text-2xl flex relative font-roboto text-transparent bg-clip-text font-medium border-b-4 bg-gradient-to-br from-red-100 to-red-500 border-b-red-300 rounded-lg">
               A simple chat app
             </p>
-            <div className='md:bg-gradient-to-br absolute bg-none left-80 top-4 h-5 w-5  rounded-full from-red-400 to-transparent' />
+            <div className='md:bg-gradient-to-br md:left-80 absolute bg-none top-4 h-5 w-5  rounded-full from-red-400 to-transparent' />
           </div>
         </div >
-        <div className='flex flex-col h-fullspace-y-1'>
+        <div className='flex flex-col w-full space-y-4'>
           <p className=" text-white font-roboto">
             Enter your username to get started!
           </p>
-          <TextField label={'username'} value={username} onChange={handleChangeUsername} />
+          <div className='flex flex-row items-center'>
+            <TextField
+              className='shadow-lg w-[50%] dark:bg-gray-800'
+              label={'Your-Username'}
+              value={username}
+              onChange={handleChangeUsername}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AccountCircleOutline className='bg-gradient-to-br from-violet-700 to-red-200 rounded-full' />
+                  </InputAdornment>
+                ),
+              }}
+            />
+            <IconButton className='w-10 h-10 ml-3'>
+              <SendCircleOutline className=' hover:text-slate-300 bg-gradient-to-br w-8 h-8 from-violet-700 to-red-200 rounded-full' />
+            </IconButton>
+          </div>
         </div>
-        <footer className="bg-white fixed bottom-4 w-[70%] justify-center rounded-lg shadow m-4 dark:bg-gray-800">
+        <footer className="bg-white fixed bottom-4 w-[50%] justify-center rounded-lg shadow m-4 dark:bg-gray-800">
           <div className="w-full mx-auto max-w-screen-xl p-4 flex content-center justify-center">
-            <span className="text-sm font-roboto text-gray-500 sm:text-center dark:text-gray-400">The account is not private; anyone can access your messages!
+            <span className="text-sm font-roboto text-gray-300 sm:text-center dark:text-gray-300">The account is public; accessible to anyone!
             </span>
           </div>
         </footer>
