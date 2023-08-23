c946568a5c27b21314bdad78e3fc123a014a4c0c
Erick <erickcestari03@gmail.com>
2023-08-23 10:12:06 -0300

    Adding message in real time

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,32 +1,68 @@
 "use client"
-import React, { useState, useEffect } from 'react'
-import{useParams, useRouter}from 'next/navigation'
-import { io } from 'socket.io-client'
-const socket = io('http://localhost:3333')
+import React, { useState, useEffect, useCallback } from 'react'
+import { useParams, useRouter } from 'next/navigation'
+import io from 'socket.io-client'
+
 const Page = () => {
   const { username } = useParams()
   const router = useRouter()
   const maxUsernameLength = 35
-  if(!username || username.length >  maxUsernameLength) router.push('/')
-  
-  const [data, setData] = useState([])
-  socket.on("connect", () => {
-    socket.emit("join", username);
-    socket.on("messages", (data) => {
-      console.log(data)
-      setData(data)
-    })
+  if (!username || username.length > maxUsernameLength) router.push('/')
 
-    socket.on("disconnect", () => {
-      console.log("Disconnected from server.");
-    });
-  });
+  const [messageList, setMessageList] = useState<any>([])
+  const [socket, setSocket] = useState<any>(null)
+  const [users, setUsers] = useState<any>([])
+  const [userNow, setUserNow] = useState<any>(null)
+  const [text, setText] = useState<string>('')
+  const [userAuthor, setUserAuthor] = useState<any>(null)
 
-  console.log(data)
+  const connectSocket = useCallback(async () => {
+    const newSocket = io('http://localhost:3333')
+    newSocket.emit('join', username)
+    newSocket.emit('userAuthor', username)
+    
+    setUserNow(users[0])
+    setSocket(newSocket)
+  }, [])
+
+  useEffect(() => {
+    connectSocket()
+  }, [connectSocket])
+
+  useEffect(() => {
+    if (socket) {
+      socket.on('messages', (messages: any) => {
+        console.log(messages)
+        setMessageList([...messages])
+      })
+      socket.on('userAuthor', (user: any) => setUserAuthor(user))
+      socket.on('users', (users: any) => setUsers([...users]))
+    }
+    
+  }, [socket])
 
 
   return (
-    <div>{username}</div>
+    <div>
+      {users.map((user: any) => <p key={user.id}>{user.name}</p>)}
+      {messageList.map((message: any) =>
+        <div key={message.id}>
+          {message.authorUser.name}: {message.content}
+        </div>
+      )}
+      <textarea
+        value={text}
+        className='w-full h-20 text-red-500'
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            e.preventDefault()
+            socket.emit('sendMessage', {content: text, authorId: "83455daa-3525-484a-b524-b6d0b52c6b9f", receiverId: "ccc2729f-cd5b-4b32-8dba-c1b8a13c9e91"})
+            setText('')
+          }
+        }}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </div>
   )
 }
 
